d68b8e344c55fff71a1869e2e96985cb9295fd99
Adrien Chinour <git@chinour.fr>
2020-11-19 23:18:39 +0100

    add amazon sns bridge for notifier component

--- src/Symfony/Component/Notifier/Transport.php
+++ src/Symfony/Component/Notifier/Transport.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Notifier;
 
 use Symfony\Component\Notifier\Bridge\AllMySms\AllMySmsTransportFactory;
 use Symfony\Component\Notifier\Bridge\Clickatell\ClickatellTransportFactory;
+use Symfony\Component\Notifier\Bridge\Amazon\AmazonTransportFactory;
 use Symfony\Component\Notifier\Bridge\Discord\DiscordTransportFactory;
 use Symfony\Component\Notifier\Bridge\Esendex\EsendexTransportFactory;
 use Symfony\Component\Notifier\Bridge\Firebase\FirebaseTransportFactory;
@@ -74,6 +75,7 @@ class Transport
         OctopushTransportFactory::class,
         GitterTransportFactory::class,
         ClickatellTransportFactory::class,
+        AmazonTransportFactory::class,
     ];
 
     private $factories;
